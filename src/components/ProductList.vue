6411ea1a9744e2c2f770c8f27496d6218a8cfecb
Your Name <you@example.com>
2022-10-06 15:53:51 +0200

    Added Product Pagination

---
+++ src/components/ProductList.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <div v-for="p in products" :key="p.id" class="card m-1 p-1 bg-light">
+      <div class="d-flex justify-content-between align-items-start">
+        <h4>
+          <p class="text-left">{{ p.name }}</p>
+          <div class="card-text bg-white p-1">
+            {{ p.description }}
+          </div>
+        </h4>
+        <div>
+          <h4>
+            <span class="badge badge-pill badge-primary float-right">
+            {{ p.price | currency }}
+          </span>
+          </h4>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<script>
+import { mapState } from "vuex"
+
+export default {
+  name: "ProductList.vue",
+  computed:{
+    ...mapState([ "products" ])
+  },
+  filters: {
+    currency(value){
+      return new Intl.NumberFormat("en-US", { style: "currency", currency: "USD"}).format(value)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/StoreComponent.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col bg-dark text-white">
+        <a href="#" class="navbar-brand">SPORT STORE</a>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-3 bg-info p-2">
+        <h4 class="text-white m-2">Categories</h4>
+      </div>
+      <div class="col-9 bg-success p-2">
+        <ProductList />
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import ProductList from "@/components/ProductList";
+
+export default {
+  components:{
+    ProductList,
+  }
+
+}
+</script>
+
+<style scoped>
+
+a.navbar-brand {
+  color: white;
+  text-align:left;
+}
+
+</style>
